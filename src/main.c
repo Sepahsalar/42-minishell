323d1a063d1df85672d82f5921b62971f828baf4
Sepahsalar <alirezasohrabizadeh@gmail.com>
2024-04-19 12:51:06 +0300

    Checking heredoc

--- src/main.c
+++ src/main.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 10:42:44 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/19 11:12:26 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/19 12:10:13 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+// remember to merge the libft in lib and in pipex
 
 int	main(int argc, char **argv, char **envp)
 {
