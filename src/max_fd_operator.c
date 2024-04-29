2f3b5a7fb1da1b5ba4994ccef3e1e77cb5eefefc
Nima Nourinejad <nnourine@c1r3p3.hive.fi>
2024-04-29 15:39:37 +0300

    Handling numbers before operator

--- src/max_fd_operator.c
+++ src/max_fd_operator.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 15:20:21 by nnourine          #+#    #+#             */
-/*   Updated: 2024/04/29 15:33:30 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:38:32 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ t_file **last_output_array(t_file *start_output)
 	int		tmp_fd;
 	int fd_counter;
 	if (!start_output)
-	    return (NULL);
+	    return (0);
 	fd_counter = 1;
 	tmp = start_output;
-	while (tmp)
-	{
-		if (tmp->)
-	}
+	// while (tmp)
+	// {
+	// 	if (tmp->)
+	// }
 	
 }
