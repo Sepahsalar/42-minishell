453787bb71ef7b1acbcdc893159568e2d0bc6bcb
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-05-10 10:26:09 +0300

    Added Null check

--- lib/libft/ft_strlen.c
+++ lib/libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:42:12 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/05/08 10:39:12 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/05/10 10:25:25 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
-	//Nima: I added this. Please check it out
 	if (!s)
 		return (0);
 	while (s[i])
