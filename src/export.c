3e98681be39d11561bd7c9bf0e80aa3eaa394df5
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-06-03 13:32:13 +0300

    cleaning up export

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:44:30 by nnourine          #+#    #+#             */
-/*   Updated: 2024/05/30 14:51:46 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/06/03 13:25:46 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,14 +63,11 @@ t_env	*sort_env(t_env *env)
 
 	index = 1;
 	count = env_count(env);
-	// printf("env_count: %d\n", count);
 	while (index <= count)
 	{
-		// printf("index: %d\n", index);
 		temp = env;
 		while (temp && temp->index)
 			temp = temp->next;
-		// printf("middle\n");
 		temp_index = temp;
 		temp = env;
 		while (temp)
@@ -80,7 +77,6 @@ t_env	*sort_env(t_env *env)
 				temp_index = temp;
 			temp = temp->next;
 		}
-		// printf("end\n");
 		if (temp_index)
 			temp_index->index = index;
 		index++;
@@ -106,166 +102,180 @@ t_env	*handle_oldpwd(t_env *env)
 	return (env);
 }
 
+void export_no_arg(t_cmd *cmd)
+{
+	int		index;
+	t_env	*sorted;
+	t_env	*cpy;
+	int		count;
+	t_env	*temp_env;
+	int		i;
+
+
+	cpy = handle_oldpwd(cpy_env(cmd->env));
+	sorted = sort_env(cpy);
+	index = 1;
+	count = env_count(cpy);
+	while (index <= count)
+	{
+		temp_env = sorted;
+		while (temp_env && temp_env->index != index)
+			temp_env = temp_env->next;
+		if (temp_env && !same(temp_env->key, "_"))
+		{
+			i = 0;
+			printf("declare -x %s", temp_env->key);
+			if (temp_env->value)
+			{
+				printf("=\"");
+				while (temp_env->value[i])
+				{
+					if (temp_env->value[i] == '\"')
+						printf("\\\"");
+					else
+						printf("%c", temp_env->value[i]);
+					i++;
+				}
+				printf("\"\n");
+			}
+			else
+				printf("\n");
+		}
+		index++;
+	}
+	clean_env_list(cpy);
+}
+
+static void print_export_error(char *arg, int *status)
+{
+	ft_putstr_fd("bash: export: `", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putendl_fd("\': not a valid identifier", 2);
+	*status = 1;
+}
+
+static void export_with_plus(t_cmd *cmd, char *arg, int *status)
+{
+	char 	*new_env;
+	char 	*temp_str_1;
+	char 	*temp_str_2;
+	char 	*find1;
+	char 	**split;
+	t_env 	*temp_env;
+	char 	*temp;
+	
+	new_env = ft_strdup(arg);
+	// if (new_env == NULL)
+	// 	return (1);
+	find1 = ft_strchr(new_env, '=');
+	temp_str_1 = ft_substr(new_env, 0, (find1 - new_env - 1));
+	if (!export_check_key(temp_str_1))
+		print_export_error(arg, status);
+	else
+	{
+		temp_str_2 = ft_substr(new_env, (find1 - new_env) + 1, ft_strlen(find1 + 1));
+		split = ft_split(new_env, '=');
+		temp_env = cmd->env;
+		while (temp_env != NULL)
+		{
+			if (ft_strlen(temp_str_1) == ft_strlen(temp_env->key)
+				&& !ft_strncmp(temp_env->key, temp_str_1,
+					ft_strlen(temp_str_1)))
+				break ;
+			temp_env = temp_env->next;
+		}
+		if (!temp_env)
+			add_node_front(&cmd->env, temp_str_1, temp_str_2);
+		else
+		{
+			temp = temp_env->value;
+			temp_env->value = ft_strjoin(temp, temp_str_2);
+			free(temp);
+		}
+		free(new_env);
+	}
+}
+
+static void export_normal(t_cmd *cmd, char *arg, int *status)
+{
+	char 	*new_env;
+	char 	*temp_str_1;
+	char 	*temp_str_2;
+	char 	*find1;
+	char 	**split;
+	t_env 	*temp_env;
+	char 	*temp;
+
+	new_env = ft_strdup(arg);
+	// if (new_env == NULL)
+	// 	return (1);
+	find1 = ft_strchr(new_env, '=');
+	temp_str_1 = ft_substr(new_env, 0, find1 - new_env);
+	if (!export_check_key(temp_str_1))
+		print_export_error(arg, status);
+	else
+	{
+		temp_str_2 = ft_substr(new_env, find1 - new_env + 1, ft_strlen(find1 + 1));
+		split = ft_split(new_env, '=');
+		temp_env = cmd->env;
+		while (temp_env != NULL)
+		{
+			if (ft_strlen(temp_str_1) == ft_strlen(temp_env->key)
+				&& !ft_strncmp(temp_env->key, temp_str_1,
+					ft_strlen(temp_str_1)))
+				break ;
+			temp_env = temp_env->next;
+		}
+		if (!temp_env)
+			add_node_front(&cmd->env, temp_str_1, temp_str_2);
+		else
+		{
+			temp = temp_env->value;
+			temp_env->value = ft_strdup(temp_str_2);
+			free(temp);
+		}
+		free(new_env);
+		//clean_2d_char(split);
+	}
+}
+
+static void export_helper(t_cmd *cmd, char *arg, int *status)
+{
+	if (!export_check(arg))
+		print_export_error(arg, status);
+	else if (ft_strchr(arg, '='))
+	{
+		if (ft_strchr(arg, '=') - (arg) > 0
+			&& *(ft_strchr(arg, '=') - 1) == '+')
+			export_with_plus(cmd, arg, status);
+		else
+			export_normal(cmd, arg, status);
+	}
+}
+
+
 t_env_pack	run_export(t_cmd *cmd)
 {
-	t_env		*temp_env;
 	t_env_pack	env_pack;
 	char		**args;
 	int			index;
-	char		*new_env;
-	char		**split;
-	char		*temp;
 	int			status;
-	t_env       *sorted;
-	t_env 		*cpy;
-	int			count;
-	int			i;
-	char *temp_str_1;
-	char *temp_str_2;
-	char *find1;
 
-	new_env = NULL;
 	status = 0;
-	i = 0;
 	args = cmd->args;
 	if (!args[1])
-	{
-		cpy = handle_oldpwd(cpy_env(cmd->env));
-		sorted = sort_env(cpy);
-		index = 1;
-		count = env_count(cpy);
-		while (index <= count)
-		{
-			temp_env = sorted;
-			while (temp_env && temp_env->index != index)
-				temp_env = temp_env->next;
-			if (temp_env && !same(temp_env->key, "_"))
-			{
-				i = 0;
-				printf("declare -x %s", temp_env->key);
-				if (temp_env->value)
-				{
-					printf("=\"");
-					while (temp_env->value[i])
-					{
-						if (temp_env->value[i] == '\"')
-							printf("\\\"");
-						else
-							printf("%c", temp_env->value[i]);
-						i++;
-					}
-					printf("\"\n");
-				}
-				else
-					printf("\n");
-			}
-			index++;
-		}
-		clean_env_list(cpy);
-	}
+		export_no_arg(cmd);
 	else
 	{
 		index = 1;
 		while (args[index])
 		{
-			// printf("hi\n");
-			if (!export_check(args[index]))
-			{
-				// printf("bash: export: `%s': not a valid identifier\n", args[index]);
-				ft_putstr_fd("bash: export: `", 2);
-				ft_putstr_fd(args[index], 2);
-				ft_putendl_fd("\': not a valid identifier", 2);
-				status = 1;
-			}
-			else if (ft_strchr(args[index], '='))
-			{
-				if (ft_strchr(args[index], '=') - (args[index]) > 0 && *(ft_strchr(args[index], '=') - 1) == '+')
-				{
-					// printf("hi\n");
-					new_env = ft_strdup(args[index]);
-					// if (new_env == NULL)
-					// 	return (1);
-					find1 = ft_strchr(new_env, '=');
-					temp_str_1 = ft_substr(new_env, 0, find1 - new_env - 1);
-					if (!export_check_key(temp_str_1))
-					{
-						ft_putstr_fd("bash: export: `", 2);
-						ft_putstr_fd(args[index], 2);
-						ft_putendl_fd("\': not a valid identifier", 2);
-						status = 1;
-					}
-					else
-					{
-						temp_str_2 = ft_substr(new_env, find1 - new_env + 1, ft_strlen(find1 + 1));
-						// printf("1: %s\n", temp_str_1);
-						// printf("2: %s\n", temp_str_2);
-						split = ft_split(new_env, '=');
-						temp_env = cmd->env;
-						while (temp_env != NULL)
-						{
-							if (ft_strlen(temp_str_1) == ft_strlen(temp_env->key)
-								&& !ft_strncmp(temp_env->key, temp_str_1,
-									ft_strlen(temp_str_1)))
-								break ;
-							temp_env = temp_env->next;
-						}
-						if (!temp_env)
-							add_node_front(&cmd->env, temp_str_1, temp_str_2);
-						else
-						{
-							temp = temp_env->value;
-							temp_env->value = ft_strjoin(temp, temp_str_2);
-							free(temp);
-						}
-						free(new_env);
-					}
-				}
-				else
-				{
-					new_env = ft_strdup(args[index]);
-					// if (new_env == NULL)
-					// 	return (1);
-					find1 = ft_strchr(new_env, '=');
-					temp_str_1 = ft_substr(new_env, 0, find1 - new_env);
-					if (!export_check_key(temp_str_1))
-					{
-						ft_putstr_fd("bash: export: `", 2);
-						ft_putstr_fd(args[index], 2);
-						ft_putendl_fd("\': not a valid identifier", 2);
-						status = 1;
-					}
-					else
-					{
-						temp_str_2 = ft_substr(new_env, find1 - new_env + 1, ft_strlen(find1 + 1));
-						split = ft_split(new_env, '=');
-						temp_env = cmd->env;
-						while (temp_env != NULL)
-						{
-							if (ft_strlen(temp_str_1) == ft_strlen(temp_env->key)
-								&& !ft_strncmp(temp_env->key, temp_str_1,
-									ft_strlen(temp_str_1)))
-								break ;
-							temp_env = temp_env->next;
-						}
-						if (!temp_env)
-							add_node_front(&cmd->env, temp_str_1, temp_str_2);
-						else
-						{
-							temp = temp_env->value;
-							temp_env->value = ft_strdup(temp_str_2);
-							free(temp);
-						}
-						free(new_env);
-						//clean_2d_char(split);
-					}
-				}
-			}
+			export_helper(cmd, args[index], &status);
 			index++;
 		}
 	}
 	env_pack.env = cmd->env;
-	// env_pack.original_env = cmd->original_env;
-	env_pack.original_env = export_original(cmd->original_env, status); // instead of 0 it should be the correct exit code
+	env_pack.original_env = export_original(cmd->original_env, status);
 	return (env_pack);
 }
 
