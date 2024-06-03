e7444d4b37e7a24101ed5a6bbcebb2727c6a7e71
Sepahsalar <alirezasohrabizadeh@gmail.com>
2024-06-03 12:20:24 +0300

    Cleaned atoi_file for norminette.

--- src/atoi_file.c
+++ src/atoi_file.c
@@ -3,115 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   atoi_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:03:14 by asohrabi          #+#    #+#             */
-/*   Updated: 2024/05/29 14:12:05 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/06/03 12:08:36 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// long	atoi_file(char **input, int def)
-// {
-// 	long	a;
-// 	char	*find;
-// 	int		temp;
-// 	int		len1;
-// 	int		len2;
-// 	char	*part2;
-// 	char	*new;
-// 	char	*str;
-// 	char	*temp_str;
-
-// 	a = 0;
-// 	str = *input;
-// 	if (def == 0)
-// 		find = ft_strchr(str, '<');
-// 	else
-// 		find = ft_strchr(str, '>');
-// 	temp = find - str;
-// 	temp--;
-// 	if ((temp >= 0 && str[temp] == '>' && def == 1)
-// 		|| (str[temp] == '<' && def == 0))
-// 		temp--;
-// 	part2 = str + temp + 1;
-// 	len2 = ft_strlen(part2);
-// 	if (str[temp] == ' ')
-// 		return (def);
-// 	while (temp >= 0 && str[temp] >= '0' && str[temp] <= '9')
-// 		temp--;
-// 	if (temp != -1 && str[temp] != ' ')
-// 		return (def);
-// 	len1 = temp + 1;
-// 	temp++;
-// 	while (str[temp] >= '0' && str[temp] <= '9')
-// 	{
-// 		a = a * 10 + (str[temp] - '0');
-// 		// if (a < 0)
-// 		// 	perror("bash");
-// 		temp++;
-// 	}
-// 	new = malloc(len1 + len2 + 1);
-// 	new[len1 + len2] = '\0';
-// 	ft_memcpy(new, str, len1);
-// 	ft_memcpy(new + len1, part2, len2);
-// 	temp_str = *input;
-// 	*input = new;
-// 	free(temp_str);
-// 	return (a);
-// }
-
-long	atoi_file(char **input, int place, int def)
+typedef struct s_atoi_file
 {
-	long	a;
-	char	*find;
-	int		temp;
-	int		len1;
-	int		len2;
-	char	*part2;
-	char	*new;
 	char	*str;
-	char	*temp_str;
+	int		place;
+	long	res;
+	int		flag;
+}			t_atoi_file;
 
-	a = 0;
-	str = *input;
-	find = str + place;
-	temp = find - str;
-	if (temp)
-		temp--;
+static t_atoi_file	atoi_file_helper(char **input, int place, int def)
+{
+	t_atoi_file	af;
+
+	af.str = *input;
+	af.flag = 0;
+	af.res = 0;
+	af.place = place;
+	if (af.place)
+		af.place--;
 	else
 	{
-		if (def == 0)
-			return (0);
-		if (def == 1)
-			return (1);
+		af.res = def;
+		af.flag = 1;
+		return (af);
 	}
-	if ((temp >= 0 && str[temp] == '>' && def == 1)
-		|| (str[temp] == '<' && def == 0))
-		temp--;
-	part2 = str + temp + 1;
-	len2 = ft_strlen(part2);
-	if (str[temp] == ' ')
-		return (def);
-	while (temp >= 0 && str[temp] >= '0' && str[temp] <= '9')
-		temp--;
-	if (temp != -1 && str[temp] != ' ')
-		return (def);
-	len1 = temp + 1;
-	temp++;
-	while (str[temp] >= '0' && str[temp] <= '9')
+	if ((af.place >= 0 && af.str[af.place] == '>' && def == 1)
+		|| (af.str[af.place] == '<' && def == 0))
+		af.place--;
+	if (af.str[af.place] == ' ')
 	{
-		a = a * 10 + (str[temp] - '0');
-		temp++;
+		af.flag = 1;
+		af.res = def;
 	}
-	new = malloc(len1 + len2 + 1);
-	new[len1 + len2] = '\0';
-	ft_memcpy(new, str, len1);
-	ft_memcpy(new + len1, part2, len2);
-	// printf("final output = %s\n", new);
+	return (af);
+}
+
+static void	atoi_file_remove(char **input, t_atoi_file	af, char *part, int len)
+{
+	char	*new;
+	char	*temp_str;
+
+	new = malloc(len + ft_strlen(part) + 1);
+	//protection
+	new[len + ft_strlen(part)] = '\0';
+	ft_memcpy(new, af.str, len);
+	ft_memcpy(new + len, part, ft_strlen(part));
 	temp_str = *input;
 	*input = new;
 	free(temp_str);
+}
+
+long	atoi_file(char **input, int place, int def)
+{
+	long		a;
+	t_atoi_file	af;
+	int			len;
+	char		*part2;
+
+	a = 0;
+	af = atoi_file_helper(input, place, def);
+	if (af.flag == 1)
+		return (af.res);
+	part2 = af.str + af.place + 1;
+	while (af.place >= 0 && af.str[af.place] >= '0' && af.str[af.place] <= '9')
+		af.place--;
+	if (af.place != -1 && af.str[af.place] != ' ')
+		return (def);
+	len = af.place + 1;
+	af.place++;
+	while (af.str[af.place] >= '0' && af.str[af.place] <= '9')
+	{
+		a = a * 10 + (af.str[af.place] - '0');
+		af.place++;
+	}
+	atoi_file_remove(input, af, part2, len);
 	return (a);
 }
