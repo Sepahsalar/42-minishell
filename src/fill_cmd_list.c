54d68a3860f6db8a57e657410ba12d6eb9d6e4b5
Sepahsalar <alirezasohrabizadeh@gmail.com>
2024-06-04 11:03:25 +0300

    Cleaned files for norminette.

--- src/fill_cmd_list.c
+++ src/fill_cmd_list.c
@@ -6,26 +6,14 @@
 /*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 09:43:39 by nnourine          #+#    #+#             */
-/*   Updated: 2024/06/03 13:57:59 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/06/04 11:02:13 by asohrabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-t_cmd	*fill_cmd_list(char **raw_cmd, t_env *env, t_env *original_env)
+static void	fill_cmd_list_helper(t_cmd *cmd, char **raw_cmd, t_env *env)
 {
-	t_cmd	*cmd;
-	int		cmd_count;
-
-	cmd_count = char_2d_count(raw_cmd);
-	cmd = create_cmd_list(cmd_count);
-	if (!cmd)
-		master_clean(raw_cmd, env, 0, EXIT_FAILURE);
-	fill_index_cmd_list(&cmd);
-	if (fill_raw_cmd_list(&cmd, raw_cmd))
-		master_clean(raw_cmd, env, cmd, EXIT_FAILURE);
-	fill_env_cmd_list(&cmd, env);
-	fill_original_env_cmd_list(&cmd, original_env);
 	if (handle_dollar_struct(cmd))
 		master_clean(raw_cmd, env, cmd, EXIT_FAILURE);
 	if (fill_files_all(&cmd))
@@ -44,6 +32,23 @@ t_cmd	*fill_cmd_list(char **raw_cmd, t_env *env, t_env *original_env)
 		master_clean(raw_cmd, env, cmd, EXIT_FAILURE);
 	if (handle_quote_cmd(cmd))
 		master_clean(raw_cmd, env, cmd, EXIT_FAILURE);
+}
+
+t_cmd	*fill_cmd_list(char **raw_cmd, t_env *env, t_env *original_env)
+{
+	t_cmd	*cmd;
+	int		cmd_count;
+
+	cmd_count = char_2d_count(raw_cmd);
+	cmd = create_cmd_list(cmd_count);
+	if (!cmd)
+		master_clean(raw_cmd, env, 0, EXIT_FAILURE);
+	fill_index_cmd_list(&cmd);
+	if (fill_raw_cmd_list(&cmd, raw_cmd))
+		master_clean(raw_cmd, env, cmd, EXIT_FAILURE);
+	fill_env_cmd_list(&cmd, env);
+	fill_original_env_cmd_list(&cmd, original_env);
+	fill_cmd_list_helper(cmd, raw_cmd, env);
 	fill_name_cmd_list(&cmd);
 	if (fill_address_access(&cmd))
 		master_clean(raw_cmd, env, cmd, EXIT_FAILURE);
