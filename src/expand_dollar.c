fbb39ab0ccab38a7f647e1e03dbe51a42c823e7f
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-06-04 13:55:47 +0300

    fill_dollar_list and expand_dollar cleaned

--- src/expand_dollar.c
+++ src/expand_dollar.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand_dollar.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:23:36 by nnourine          #+#    #+#             */
-/*   Updated: 2024/06/03 13:56:34 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/06/04 13:11:18 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,103 +85,146 @@ char	*get_current_pid(t_env *original_env)
 	return (pid_str);
 }
 
-char	*expand_dollar_helper(t_cmd *cmd, char *str , char *find, int type)
+void expand_two_dollars(t_cmd *cmd, char **str, char **find, int *remained_dollar)
 {
 	t_env	*env;
-	char	*new_str;
-	char	*expanded;
-	int		len_var;
-	int		len1;
-	char	*part2;
-	char	*variable;
 	int		index;
 	char	*temp;
 	int		count;
 	int		initial_length;
-	int		remained_dollar;
 	int		reletive_index;
 
 	env = cmd->env;
-	temp = str;
 	count = 0;
 	index = 0;
-	initial_length = ft_strlen(str);
+	initial_length = ft_strlen((*str));
 	temp = cmd->original_env->next->value;
-	while (find[index] && find[index] == '$')
+	while ((*find)[index] && (*find)[index] == '$')
 	{
 		count++;
 		index++;
 	}
-	remained_dollar = count % 2;
+	(*remained_dollar) = count % 2;
 	count = count / 2;
-	reletive_index = find - str;
-	str = expand_pid(str, find, temp, count);
+	reletive_index = (*find) - (*str);
+	(*str) = expand_pid((*str), (*find), temp, count);
 	if (count)
-		find = str + reletive_index + (ft_strlen(temp) * count);
-	if ((!*find) || (find && (*(find + 1) == '\0'
+		(*find) = (*str) + reletive_index + (ft_strlen(temp) * count);
+}
+
+int	no_need_more_expand(int remain_dollar, char *find, int type)
+{
+	if (!remain_dollar)
+		return (1);
+	return ((!*find) || (find && (*(find + 1) == '\0'
 				|| *(find + 1) == ' ' || *(find + 1) == '='
 				|| (type == 2 && (*(find + 1) == '\''
-						|| *(find + 1) == '\"')))))
+						|| *(find + 1) == '\"')))));
+}
+
+int	it_is_var(char *find, int len_var)
+{
+	if (find[len_var] && find[len_var] != ' '
+		&& find[len_var] != '\"' && find[len_var] != '\''
+		&& find[len_var] != '?' && find[len_var] != '=')
+		return (1);
+	return (0);
+}
+
+int	count_len_var(char *find)
+{
+	int	len_var;
+
+	len_var = 0;
+	while (it_is_var(find, len_var))
+		len_var++;
+	
+	if (find[0] == '?' && len_var == 0)
+		len_var = 1;
+	return (len_var);
+}
+
+char *find_var(char *find)
+{
+	char	*variable;
+	int		len_var;
+
+	len_var = count_len_var(find);
+	variable = malloc(len_var + 1);
+	if (!variable)
+		return (NULL);
+	ft_memcpy(variable, find, len_var);
+	variable[len_var] = '\0';
+	return (variable);
+}
+
+char *find_expanded(t_cmd *cmd, char *variable)
+{
+	char	*expanded;
+
+	if (same(variable, "?"))
+		expanded = ft_strdup(cmd->original_env->value);
+	else
 	{
-		new_str = ft_strdup(str);
-		return (new_str);
-	}
-	if (remained_dollar)
-	{
-		len1 = find - str;
-		find++;
-		len_var = 0;
-		while (find[len_var] && find[len_var] != ' '
-			&& find[len_var] != '\"' && find[len_var] != '\''
-			&& find[len_var] != '?' && find[len_var] != '=')
-			len_var++;
-		if (find[0] == '?' && len_var == 0)
-			len_var = 1;
-		variable = malloc(len_var + 1);
-		if (!variable)
-			return (NULL);
-		ft_memcpy(variable, find, len_var);
-		variable[len_var] = '\0';
-		part2 = find + len_var;
-		expanded = NULL;
-		if (same(variable, "?"))
-		{
-			if (same(cmd->original_env->key, "exit_code"))
-				expanded = ft_strdup(cmd->original_env->value);
-		}
-		else
-		{
-			while (env && ((int)ft_strlen(env->key) != len_var
-					|| ft_strncmp((env->key), variable, len_var)))
-				env = env->next;
-			if (env)
-				expanded = ft_strdup(env->value);
-			else
-				expanded = ft_strdup("");
-			if (!expanded)
-			{
-				free(variable);
-				return (NULL);
-			}
-		}
-		new_str = malloc(len1 + ft_strlen(expanded) + ft_strlen(part2) + 1);
-		if (!new_str)
+		expanded = ft_strdup(value_finder(cmd->env, variable));
+		if (!expanded)
+			expanded = ft_strdup("");
+		if (!expanded)
 		{
 			free(variable);
-			free(expanded);
 			return (NULL);
 		}
-		new_str[len1 + ft_strlen(expanded) + ft_strlen(part2)] = '\0';
-		ft_memcpy(new_str, str, len1);
-		ft_memcpy(new_str + len1, expanded, ft_strlen(expanded));
-		ft_memcpy(new_str + len1
-			+ ft_strlen(expanded), part2, ft_strlen(part2));
+	}
+	return (expanded);
+}
+
+char *str_after_expansion(char *str, char *find, char *variable, char *expanded)
+{
+	char	*new_str;
+	char	*part2;
+	int     len1;
+
+	len1 = find - str -1;
+	part2 = find + ft_strlen(variable);
+	new_str = malloc(len1 + ft_strlen(expanded) + ft_strlen(part2) + 1);
+	if (!new_str)
+	{
 		free(variable);
 		free(expanded);
+		return (NULL);
 	}
-	else
-		new_str = ft_strdup(str);
+	new_str[len1 + ft_strlen(expanded) + ft_strlen(part2)] = '\0';
+	ft_memcpy(new_str, str, len1);
+	ft_memcpy(new_str + len1, expanded, ft_strlen(expanded));
+	ft_memcpy(new_str + len1
+		+ ft_strlen(expanded), part2, ft_strlen(part2));
 	return (new_str);
+}
+
+
+char *expand_one_dollar(t_cmd *cmd, char *str , char *find)
+{
+	char	*new_str;
+	char	*expanded;
+	char	*variable;
+
+	find++;
+	variable = find_var(find);
+	expanded = find_expanded(cmd, variable);
+	new_str = str_after_expansion(str, find, variable, expanded);
+	free(variable);
+	free(expanded);
+	return (new_str);
+}
+
+char	*expand_dollar_helper(t_cmd *cmd, char *str , char *find, int type)
+{
+	int		remained_dollar;
+
+	expand_two_dollars(cmd, &str, &find, &remained_dollar);
+	if (no_need_more_expand(remained_dollar, find, type))
+		return (ft_strdup(str));
+	return (expand_one_dollar(cmd, str, find));
 }
 
 char	*inside_double_quote(char *location)
@@ -202,21 +245,30 @@ char	*inside_double_quote(char *location)
 	return (inside);
 }
 
+char *replace_inside_helper(char *str, char *inside, char *handled)
+{
+	char	*new_str;
+	int		new_len;
+
+	new_len = ft_strlen(str) - ft_strlen(inside) + ft_strlen(handled);
+	new_str = malloc(new_len + 1);
+	new_str[new_len] = '\0';
+	return (new_str);
+}
+
 char	*replace_inside(char *str, char *location, char *inside, char *handled)
 {
 	char	*new_str;
 	char	*dst;
 	char	*src;
 	int		l;
-	int		new_len;
 
-	new_len = ft_strlen(str) - ft_strlen(inside) + ft_strlen(handled);
-	new_str = malloc(new_len + 1);
-	new_str[new_len] = '\0';
+	new_str = replace_inside_helper(str, inside, handled);
 	dst = new_str;
 	src = str;
 	l = location - str + 1;
 	ft_memcpy(dst, src, l);
+	ft_memcpy(new_str, str, (location - str + 1));
 	dst = dst + l;
 	src = handled;
 	l = ft_strlen(handled);
