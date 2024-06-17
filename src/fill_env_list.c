7e1a43f1f657f902f38814deddf9cc6890a6f7f8
nima-nourinejad <nima.nourinejad@gmail.com>
2024-06-17 17:36:49 +0300

    Protected fill_env_list file.

--- src/fill_env_list.c
+++ src/fill_env_list.c
@@ -6,7 +6,7 @@
 /*   By: nnourine <nnourine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:34:31 by nnourine          #+#    #+#             */
-/*   Updated: 2024/06/17 14:37:20 by nnourine         ###   ########.fr       */
+/*   Updated: 2024/06/17 17:35:36 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,27 @@ t_env	*fill_env_list(char **envp)
 
 	env_count = char_2d_count(envp);
 	env = create_env_list(env_count);
+	if (!env)
+		return (NULL);
 	index = 0;
 	temp_env = env;
 	while (index < env_count)
 	{
 		temp = ft_split(envp[index], '=');
-		// if (!temp)
-		// 	master_clean(raw_cmd, env, 0, EXIT_FAILURE);
+		if (!temp)
+		{
+			clean_env_list(env);
+			return (NULL);
+		}
 		temp_env->key = ft_strdup(temp[0]);
-		temp_env->value = ft_substr(envp[index], ft_strlen(temp[0]) + 1	, ft_strlen(envp[index] + ft_strlen(temp[0]) + 1));
-		// temp_env->value = ft_strdup(temp[1]);
+		temp_env->value = ft_substr(envp[index], ft_strlen(temp[0]) + 1,
+			ft_strlen(envp[index] + ft_strlen(temp[0]) + 1));
 		clean_2d_char(temp);
-		// free (temp);
-		// if (!temp_env->key || !temp_env->value)
-		// 	master_clean(raw_cmd, env, 0, EXIT_FAILURE);
+		if (!temp_env->key || !temp_env->value)
+		{
+			clean_env_list(env);
+            return (NULL);
+        }
 		temp_env = temp_env->next;
 		index++;
 	}
