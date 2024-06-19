4f243b2efaf169d35401b8f78a0d5e619b6be5d3
nima-nourinejad <nima.nourinejad@gmail.com>
2024-06-19 13:20:34 +0300

    cd norm is done

--- src/cd.c
+++ src/cd.c
@@ -3,20 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asohrabi <asohrabi@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: nnourine <nnourine@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 13:56:25 by nnourine          #+#    #+#             */
-/*   Updated: 2024/06/19 11:09:35 by asohrabi         ###   ########.fr       */
+/*   Updated: 2024/06/19 13:17:44 by nnourine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char	*full_path_finder(char *pwd, char *arg, char *home)
+char	*full_path_finder(char *pwd, char *arg, t_cmd *cmd)
 {
 	char	*temp1;
 	char	*temp2;
+	char	*home;
 
+	home = value_finder(cmd->original_env, "HOME");
 	if (!arg)
 		return (ft_strdup(home));
 	else if (arg[0] == '~')
@@ -38,6 +40,8 @@ t_env_pack	run_cd_helper(char *full_path, t_cmd *cmd, t_env_pack env_pack)
 {
 	char		*new_pwd;
 
+	if (!full_path)
+		master_clean(NULL, cmd, EXIT_FAILURE);
 	if (chdir(full_path) == -1)
 	{
 		ft_putstr_fd("bash: ", 2);
@@ -57,40 +61,20 @@ t_env_pack	run_cd_helper(char *full_path, t_cmd *cmd, t_env_pack env_pack)
 		free(new_pwd);
 		env_pack.original_env = export_original(cmd->original_env, 0);
 	}
+	free(full_path);
 	return (env_pack);
 }
 
-int	start_with_double_dot(char *arg)
+char	*del_folder_double_dot(t_cmd *cmd, t_env_pack env_pack)
 {
-	if (!arg || ft_strlen(arg) <= 2)
-		return (0);
-	if (arg[0] == '.' && arg[1] == '.')
-		return (1);
-	return (0);
-}
-
-char	*path_start_with_double_dot(t_cmd *cmd, t_env_pack env_pack)
-{
-	char	*old_pwd;
-	char	*part_one;
-	char	*part_two;
-	char	*full_path;
-
-	old_pwd = value_finder(env_pack.env, "PWD");
-	part_one = sliced_str(old_pwd, 0,
-			(ft_strrchr(old_pwd, '/') - old_pwd));
-	if (!part_one)
-		return (NULL);
-	part_two = sliced_str(cmd->args[1], 2, ft_strlen(cmd->args[1]));
-	if (!part_two)
-	{
-		free(part_one);
-		return (NULL);
-	}
-	full_path = ft_strjoin(part_one, part_two);
-	free(part_one);
-	free(part_two);
+	char *full_path;
+	
+	if (cmd->args[1] && same(cmd->args[1], ".."))
+		full_path = path_only_one_double_dot(env_pack);
+	else
+		full_path = path_start_with_double_dot(cmd, env_pack);
 	return (full_path);
+	
 }
 
 t_env_pack	run_cd(t_cmd *cmd)
@@ -98,60 +82,26 @@ t_env_pack	run_cd(t_cmd *cmd)
 	char		*old_pwd;
 	t_env_pack	env_pack;
 	char		*full_path;
-	char		*home;
 
-	full_path = NULL;
 	env_pack = init_env_pack(cmd);
-	// if (cmd->args[2])
-	// {
-    //     ft_putendl_fd("bash: cd: too many arguments", 2);
-    //     env_pack.original_env = export_original(cmd->original_env, 1);
-    //     return (env_pack);
-    // }
 	old_pwd = getcwd(NULL, 0);
 	if (!old_pwd)
 	{
 		if (same(cmd->args[1], ".") && errno == ENOENT)
-		{
-			ft_putstr_fd("cd: error retrieving current directory: ", 2);
-			ft_putstr_fd("getcwd: cannot access parent directories: ", 2);
-			ft_putendl_fd(strerror(ENOENT), 2);
-			env_pack.original_env = export_original(cmd->original_env, 0);
-			return (env_pack);
-		}
-		else if (cmd->args[1] && same(cmd->args[1], ".."))
-		{
-			old_pwd = value_finder(env_pack.env, "PWD");
-			full_path = sliced_str(old_pwd, 0,
-					(ft_strrchr(old_pwd, '/') - old_pwd));
-		}
+		    return (del_folder_one_dot(env_pack, cmd));
 		else if (start_with_double_dot(cmd->args[1]))
-			full_path = path_start_with_double_dot(cmd, env_pack);
+			full_path = del_folder_double_dot(cmd, env_pack);
 		else if (cmd->args[1] == NULL)
 			full_path = ft_strdup(value_finder(cmd->original_env, "HOME"));
 		else
-		{
-			ft_putstr_fd("bash: ", 2);
-			ft_putstr_fd(cmd->args[0], 2);
-			ft_putstr_fd(": ", 2);
-			ft_putstr_fd(cmd->args[1], 2);
-			ft_putstr_fd(": ", 2);
-			ft_putendl_fd(strerror(errno), 2);
-			env_pack.original_env = export_original(cmd->original_env, 1);
-			return (env_pack);
-		}
-		//old_pwd = ft_strdup(value_finder(env_pack.env, "PWD"));
+			return (del_folder_else(env_pack, cmd));
 	}
 	else
 	{
 		env_pack.env = custom_export(env_pack.env, "OLDPWD", old_pwd);
-		home = value_finder(cmd->original_env, "HOME");
-		full_path = full_path_finder(old_pwd, cmd->args[1], home);
+		full_path = full_path_finder(old_pwd, cmd->args[1], cmd);
 		free(old_pwd);
 	}
-	if (!full_path)
-		master_clean(NULL, cmd, EXIT_FAILURE);
 	env_pack = run_cd_helper(full_path, cmd, env_pack);
-	free(full_path);
 	return (env_pack);
 }
